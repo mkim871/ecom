ddc76b1b0aadaff4f63cc78e128f5b81291cc421
Nicholas_PC <5-Seaters>
2020-04-07 00:50:29 +1200

    [FIX] Not signed in error fix

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -6,14 +6,19 @@ import { auth } from "../../firebase";
 import Home from "../Home/Home";
 import Auth from "../Auth/Auth";
 import Alert from "../Alert/Alert";
+import Lists from "../Lists/Lists";
 import * as ac from "../Auth/actions";
 import userService from "../../_services/user.service";
 
 class App extends Component {
   componentDidMount() {
     this.listener = auth.onAuthStateChanged(async auth => {
-      const user = await userService.getUserDetail(auth.uid);
-      this.props.onAuth(user);
+      if (auth) {
+        const user = await userService.getUserDetail(auth.uid);
+        this.props.onAuth(user);
+      } else {
+        this.props.onAuth(null);
+      }
     });
   }
 
@@ -28,9 +33,11 @@ class App extends Component {
         <Router history={history}>
         <Link to="/auth/signin">Sign in</Link>
         <Link to="/auth/register">Register</Link>
+        <Link to="/lists">Lists</Link>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/auth/:type" component={Auth} />
+            <Route path="/lists" component={Lists} />
             <Route render={() => <div>Not found</div>}></Route>
           </Switch>
         </Router>
