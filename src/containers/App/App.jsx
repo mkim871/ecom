ad3d309284a608d42c3a1661a9056487b56b48a8
Nicholas_PC <5-Seaters>
2020-04-06 22:20:16 +1200

    [MOD] Minor edits

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -3,12 +3,10 @@ import { connect } from "react-redux";
 import { Router, Switch, Route, Link } from "react-router-dom";
 import { history } from "../../_helpers/history";
 import { auth } from "../../firebase";
-import logo from "../../logo.svg";
-import "./App.css";
 import Home from "../Home/Home";
 import Auth from "../Auth/Auth";
 import Alert from "../Alert/Alert";
-import * as ac from '../Auth/actions';
+import * as ac from "../Auth/actions";
 import userService from "../../_services/user.service";
 
 class App extends Component {
@@ -21,36 +19,22 @@ class App extends Component {
 
   componentWillUnmount() {
     this.listener();
-  }  
-  
+  }
+
   render() {
     return (
-      <Router history={history}>
-        <div className="App">
-          {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-          <Alert />
-          <Link to="/auth/signin">Sign in</Link>
-          <Link to="/auth/register">Register</Link>
+      <React.Fragment>
+        <Alert />
+        <Router history={history}>
+        <Link to="/auth/signin">Sign in</Link>
+        <Link to="/auth/register">Register</Link>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/auth/:type" component={Auth} />
             <Route render={() => <div>Not found</div>}></Route>
           </Switch>
-        </div>
-      </Router>
+        </Router>
+      </React.Fragment>
     );
   }
 }
@@ -63,7 +47,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (auth) => dispatch(ac.updateAuth(auth))
+    onAuth: auth => dispatch(ac.updateAuth(auth))
   };
 };
 
